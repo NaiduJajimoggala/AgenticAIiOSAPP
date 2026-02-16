4ea41a31a0f30da8a31ca137ab3542e016b5b1ac
A S Naidu <42375531+NaiduJajimoggala@users.noreply.github.com>
2026-02-16 12:15:28 +0530

    Add files via upload
    Agentic Ai Code

---
+++ AgenticAgent/App/ReActAgentApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by swami Naidu on 26/07/2025
+// Copyright Apps Bay Limited. All rights reserved.
+//
+  
+
+import SwiftUI
+
+@main
+struct ReActAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
